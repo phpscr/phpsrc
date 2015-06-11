8a23c69b7f116fafcdf12db71f85e128c1981895
Uahoo <uahoo@sina.com>
2015-06-11 15:42:14 +0800

    file

---
+++ phplanguage/stdout/stdout.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * 提示用户输入，类似Python
+ */
+ 
+$fs = true;
+ 
+do{
+if($fs){
+fwrite(STDOUT,'请输入您的博客名：');
+$fs = false;
+}else{
+fwrite(STDOUT,'抱歉，博客名不能为空，请重新输入您的博客名：');
+}
+ 
+$name = trim(fgets(STDIN));
+ 
+}while(!$name);
+ 
+echo '您输入的信息是：'.$name."\r\n";
