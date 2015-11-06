a97fd2c9ee49879c672c47a6c58f172bdf98875c
Uahoo <uahoo@sina.com>
2015-11-06 15:30:32 +0800

    .

--- php-learn/strops.php
+++ php-learn/strops.php
@@ -1,3 +1,10 @@
 <?php
-echo strpos("You love php, I love php too!","php");
+//echo strpos("You love php, I love php too!","php");
+print strpos("You love php, I love php too!","php");
+if(strpos("lixin@staff.sina.com.cn","@") !== false){
+echo "包";
+}else{
+echo "错";
+}
+
 ?>
