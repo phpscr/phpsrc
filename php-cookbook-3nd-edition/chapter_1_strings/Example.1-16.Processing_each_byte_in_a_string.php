a7949bf1424fcbad67853202334a9ba35fe5d8b6
Uahoo <uahoo@sina.com>
2015-11-10 17:29:18 +0800

    strstr

--- php-cookbook-3nd-edition/chapter_1_strings/Example.1-16.Processing_each_byte_in_a_string.php
+++ php-cookbook-3nd-edition/chapter_1_strings/Example.1-16.Processing_each_byte_in_a_string.php
@@ -1,9 +1,23 @@
-string.
-Example 1-16. Processing each byte in a string
+
+<?php
+//Example 1-16. Processing each byte in a string
 $string = "This weekend, I'm going shopping for a pet chicken.";
 $vowels = 0;
 for ($i = 0, $j = strlen($string); $i < $j; $i++) {
-if (strstr('aeiouAEIOU',$string[$i])) {
-$vowels++;
+	echo "\n";
+	echo $i;
+	echo "\n";
+	echo $j;
+	echo "\n";
+	if (strstr('aeiouAEIOU',$string[$i])) {
+	$vowels++;
+	}
 }
-}
+
+print "\n";
+print $string;
+print "\n";
+print $vowels;
+
+?>
+
