5454eb52dc9c5df3e892902e6435e412af95d91b
Uahoo <uahoo@sina.com>
2015-11-19 15:00:39 +0800

    update to

--- php-func/array/array.php
+++ php-func/array/array.php
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-<?php
-=======
 /*
->>>>>>> origin/HEAD
  $array=array("1"=>"编","2"=>"程","3"=>"词","4"=>"典");
  print_r($array);
  echo "<br>";
@@ -10,15 +6,10 @@
  echo $array[2];
  echo $array[3];
  echo $array[4];
-<<<<<<< HEAD
- echo "<br>";
-?>
-=======
 */
 <?php
  $array[1]="I";
  $array[2]="love";
  $array[3]="PHP";
  print_r($array); //输出所创建数组的结构
- ?>
->>>>>>> origin/HEAD
+?>
