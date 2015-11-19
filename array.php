41aaf9ec8cef06608666c5b70e6171b0e38446e0
gitsvn <uahoo@sina.com>
2015-11-19 14:15:00 +0800

    array

---
+++ array.php
@@ -0,0 +1,9 @@
+，<?php
+ $array=array("1"=>"编","2"=>"程","3"=>"词","4"=>"典");
+ print_r($array);
+ echo "<br>";
+ echo $array[1]; //注意：下标默认是从0开始
+ echo $array[2];
+ echo $array[3];
+ echo $array[4];
+ ?>
