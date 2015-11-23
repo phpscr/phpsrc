e79ba1439a997cd55a4cb31afc9edacd362b3f6f
Uahoo <uahoo@sina.com>
2015-11-23 19:01:51 +0800

    b

--- php-func/array/statistical_number_of_letters.php
+++ php-func/array/statistical_number_of_letters.php
@@ -1,14 +1,25 @@
 //统计文件中的字母个数
 <?php
 
-
+$n=0;
 $handle = @fopen("/data0/largefile.random.txt","r");
+var_dump($handle);
 if($handle){
-  while(!feof($handle)){
-    $string = fgets($handle);
-  }
-  for($i=0,$j=strlen($string);$i<$j;$i++){
-    echo "$string[$i]";
+	while(!feof($handle)){
+  	$string = fgets($handle);
+	#print($string);
+   	#var_dump($string);
+  
+  		for($i=0,$j=strlen($string);$i<$j;$i++){
+			#var_dump($i,$j);
+			#echo "$string[$i]";
+			#echo "\n";
+			$n=$n+1;
+			var_dump($n);			
 
-  }
+  		}
+	}
 }
+echo "=======" ;
+echo $n ;
+echo "=======" ;
