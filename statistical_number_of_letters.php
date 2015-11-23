ad4b4dc8b7db82a945c1d99f8271ba1d35dd2cfa
gitsvn <uahoo@sina.com>
2015-11-23 16:17:41 +0800

    two dimension array

---
+++ statistical_number_of_letters.php
@@ -0,0 +1,14 @@
+//统计文件中的字母个数
+<?php
+
+
+$handle = @fopen("/data0/largefile.random.txt","r");
+if($handle){
+  while(!feof($handle)){
+    $string = fgets($handle);
+  }
+  for($i=0,$j=strlen($string),$i<$j){
+    echo "$string[$i]";
+    
+  }
+}
