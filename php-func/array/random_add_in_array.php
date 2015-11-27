cc1be5eeac5c6da838785739fae7b3c77dfa4df6
svngit <uahoo>
2015-11-27 11:11:24 +0000

    function

---
+++ php-func/array/random_add_in_array.php
@@ -0,0 +1,33 @@
+<?php
+//random
+
+
+/*
+$arr=array(
+	'bj'=>array(
+		'name'=>'lx',
+		'address'=>'beijing donglu',
+		'phone'=>'12345678900'
+));	
+
+print_r($arr);
+
+*/
+
+$myNum=100; 
+$number=0;
+function Valuechange ($number) 
+{ 
+	$number=$number+1; 
+	Echo "$number \n" ; 
+}
+for($i=0;$i<99999;$i++){
+ 
+	Valuechange($myNum); 
+//	echo $i;
+//	echo $myNum; 
+}
+	echo $i;
+#	echo "\n";
+#	echo $number;
+?> 
