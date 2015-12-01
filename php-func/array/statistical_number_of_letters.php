3a0ddbf07ee209a8330d1196157b11c07dd61147
gitsvn <uahoo@sina.com>
2015-12-01 16:00:17 +0800

    t

--- php-func/array/statistical_number_of_letters.php
+++ php-func/array/statistical_number_of_letters.php
@@ -16,26 +16,33 @@ $count_letter = array(
 	);
 print_r($count_letter);
 
+
+////
+$file_arr = file()
+///
+
 $n=0;
 $handle = @fopen("/data0/largefile.random.txt","r");
+
+
 var_dump($handle);
 if($handle){
 	while(!feof($handle)){
   	$string = fgets($handle);
 	#print($string);
    	#var_dump($string);
-  
+
   		for($i=0,$j=strlen($string);$i<$j;$i++){
 			#var_dump($i,$j);
 			#echo "$string[$i]";
 			#echo "\n";
 			$n=$n+1;
-		#	var_dump($n);			
+		#	var_dump($n);
 			foreach($count_letter as $k => $val) {
-			
+
 				if (string[$i] === $val['0']){
 					$val['1']=$val['1']+1;
-					$count_letter=array(array('1'=>$val[$i]));	
+					$count_letter=array(array('1'=>$val[$i]));
 
 
   		}
