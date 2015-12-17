ab28030d1908da8800460fb2c36c976d7fba4242
Uahoo <uahoo@sina.com>
2015-12-17 16:00:28 +0800

    t

--- php-func/array/two-way-array.php
+++ php-func/array/two-way-array.php
@@ -1,14 +1,34 @@
 <?php
-     $number = array(1,3,5,7,9);
-     //定义空数组
-     $result = array();
-     $color =array("red","blue","green");
-     //自定义键值
-     $language = (1=>"English",3=>"Chinese",5=>"Franch");
-     //定义二维数组
-     $two = array(
-                 "color"=>array("red","blue"),    //用逗号结尾
+/*
+	
+	$number = array(1,3,5,7,9);
+	//定义空数组
+	$result = array();
+     	$color =array("red","blue","green");
+     	//自定义键值
+     	$language = array(1=>"English",3=>"Chinese",5=>"Franch");
+     	//定义二维数组
+     	$two = array(
+       	          "color"=>array("red","blue"),    //用逗号结尾
                  "week"=>array("Monday","Friday")    //最后一句没有标点
-     )；
-    print_r $two;
+     );
+ 	print_r ($number);
+	print_r ( $two );
  ?>
+*/
+
+//<?php
+
+$arr = array(
+	"A" => 0, "B" => 0, "C" => 0, "D" => 0, "E" => 0, "F" => 0, 
+	"F" => 0, "G" => 0, "H" => 0, "I" => 0, "J" => 0, "K" => 0
+);
+
+echo $arr["A"]; // 
+echo "\n";
+echo $arr["F"];    // 1
+echo "\n" ;
+print_r ($arr );
+
+?>
+ 
