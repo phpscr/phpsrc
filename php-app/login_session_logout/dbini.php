eca30b9277eda949dec4fc11c7e719f2cbc50646
uahoo <uahoo@outlook.com>
2016-08-16 17:53:47 +0800

    session

---
+++ php-app/login_session_logout/dbini.php
@@ -0,0 +1,11 @@
+
+ <?php
+  //数据库的位置
+  define('DB_HOST', 'localhost');
+   //用户名
+   define('DB_USER', 'root');
+    //口令
+    define('DB_PASSWORD', '19900101');
+     //数据库名
+     define('DB_NAME','test') ;
+      ?>
