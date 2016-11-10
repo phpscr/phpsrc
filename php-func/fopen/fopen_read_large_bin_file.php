6ac64da4ce6dba5381f7ce7019455440bd68c05d
lixin <gitsvn@yahoo.com>
2016-11-10 17:49:20 +0800

    fd

---
+++ php-func/fopen/fopen_read_large_bin_file.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * 读写大二进制文件，不必申请很大内存
+ * 只有读取到内容才创建文件
+ * 保证目录可写
+ *
+ * @param string $srcPath 源文件路径
+ * @param string $dstPath 目标文件路径
+ * @return bool
+ */
+function fetch_big_file($srcPath, $dstPath)
+{
+    set_time_limit(0); // 设置脚本执行时间无限长
+ 
+    if (!$fpSrc = fopen($srcPath, "rb"))
+    {
+        return false;
+    }
+ 
+    $isWriteFileOpen = false; // 写文件 是否已打开？
+    do
+    {
+        $data = fread($fpSrc, 8192); // 每次读取 8*1024个字节
+        if (!$data)
+        {
+            break;
+        }
+        else if (!$isWriteFileOpen)
+        {
+            // 第一次读取文件，并且有内容，才创建文件
+            $fpDst = fopen($dstPath, "wb");
+            $isWriteFileOpen = true;
+            fwrite($fpDst, $data);
+        }
+        else
+        {
+            // 写入
+            fwrite($fpDst, $data);
+        }
+    } while (true);
+ 
+    fclose($fpSrc);
+    fclose($fpDst);
+ 
+    return true;
+}
+ 
+//$srcPath = 'd:/PHP/data/eclipse-jee-kepler-R-win32-x86_64.pdf';
+$srcPath = 'http://dl.bintray.com/gitblit/releases/gitblit-1.8.0.zip';
+
+$dstPath = './gitblit-1.8.0.zip';
+ 
+fetch_big_file($srcPath, $dstPath);
+ 
+echo 'success';
---
+++ php-func/md5/t.php
@@ -0,0 +1,8 @@
+<?php 
+date_default_timezone_set('PRC');
+ print strftime('%c'); 
+ print "\n"; 
+ print date('Ymd His'); 
+ ?> 
+
+
